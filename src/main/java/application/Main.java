6b91b3d30d214fb27646ba63ac28f7d28b54ae14
juan gabriel <juanbogobenitez@gmail.com>
2025-03-26 15:05:51 -0300

    implementação classe Seller

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,11 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
         Department dep = new Department(1,"Books");
         System.out.println(dep);
+
+        Seller seller = new Seller(1, "Bob", "bob@gmail.com", LocalDate.now(),4500.5, dep );
+        System.out.println(seller);
     }
 }
